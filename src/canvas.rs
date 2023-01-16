c696dc319dbb2af74d965dcec9e15292f98770d3
alexanderved <avedehin692@gmail.com>
2023-01-16 18:52:26 +0300

    Change line drawing algorithm

--- src/canvas.rs
+++ src/canvas.rs
@@ -15,45 +15,52 @@ impl Canvas {
         let mut p0 = line.p0;
         let mut p1 = line.p1;
 
-        if (p1.x - p0.x).abs() >= (p1.y - p0.y).abs() {
+        if (p1.x - p0.x).abs() > (p1.y - p0.y).abs() {
             if p0.x == p1.x { return; }
 
             if p0.x > p1.x {
                 mem::swap(&mut p0, &mut p1);
             }
 
-            let mut y = p0.y;
+            let x0 = p0.x.floor();
+            let x1 = p1.x.ceil().min(self.width as f32);
+
             let k = (p1.y - p0.y) / (p1.x - p0.x);
-            for x in p0.x as usize..self.width.min(p1.x.ceil() as usize) {
-                let row = y as usize * self.width;
+            let mut y = p0.y + k * (x0 - p0.x);
 
-                self.bitmap[row + x] = 1.0 - (y - y.floor());
-                self.bitmap[row + self.width + x] = y - y.floor();
+            for x in x0 as usize..x1 as usize {
+                self.plot(x, y as usize, 1.0 - (y - y.floor()));
+                self.plot(x, y as usize + 1, y - y.floor());
 
-                let dx = 1.0_f32.min(self.width as f32 - p1.x);
-
-                y += k * dx;
+                y += k;
             }
-        } else if (p1.x - p0.x).abs() < (p1.y - p0.y).abs() {
+        } else if (p1.x - p0.x).abs() <= (p1.y - p0.y).abs() {
             if p0.y == p1.y { return; }
 
             if p0.y > p1.y {
                 mem::swap(&mut p0, &mut p1);
             }
 
-            let mut x = p0.x;
-            let k = (p1.x - p0.x) / (p1.y - p0.y);
-            for y in p0.y as usize..self.height.min(p1.y.ceil() as usize) {
-                let row = y * self.width;
+            let y0 = p0.y.floor();
+            let y1 = p1.y.ceil().min(self.height as f32);
 
-                self.bitmap[row + x as usize] = 1.0 - (x - x.floor());
-                self.bitmap[row + x as usize + 1] = x - x.floor();
+            let k = (p1.x - p0.x) / (p1.y - p0.y);
+            let mut x = p0.x + k * (y0 - p0.y);
+
+            for y in y0 as usize..y1 as usize {
+                self.plot(x as usize, y, 1.0 - (x - x.floor()));
+                self.plot(x as usize + 1, y, x - x.floor());
 
                 let dy = 1.0_f32.min(self.height as f32 - p1.y);
 
                 x += k * dy
             }
         }
+    }
+
+    #[inline(always)]
+    fn plot(&mut self, x: usize, y: usize, c: f32) {
+        self.bitmap[y * self.width + x] = c;
     }
 }
 
@@ -85,13 +92,13 @@ impl CanvasBuilder {
             bitmap: vec![0.0; self.width * self.height]
         };
 
-        self.lines.iter().for_each(|line| {
-            canvas.draw_line(line);
-        });
-
         self.lines.sort_by(|left, right|
             left.p0.y.partial_cmp(&right.p0.y).unwrap()
         );
+
+        self.lines.iter().for_each(|line| {
+            canvas.draw_line(line);
+        });
 
         let mut hits = Vec::with_capacity(16);
 
