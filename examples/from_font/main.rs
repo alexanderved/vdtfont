e2dbe2afe6d44710fcd1dee41e978454ad9c8588
alexanderved <avedehin692@gmail.com>
2023-02-07 17:14:49 +0300

    Revert "Add benchmarking"
    This reverts commit 61127d910c6d1afcc0fceeefd2852416fe9c3e23.

--- examples/from_font/main.rs
+++ examples/from_font/main.rs
@@ -6,30 +6,13 @@
 
 mod example;
 mod outliner;
- 
-use std::{time, hint};
+
 use crate::example::*;
- 
-const BENCH_COUNT: usize = 10000;
- 
-fn bench(example: &dyn Example) -> time::Duration {
-    let now = time::Instant::now();
-    let f = || (0..BENCH_COUNT).into_iter()
-        .for_each(|_| { example.build_canvas(); });
- 
-    hint::black_box(f());
- 
-    now.elapsed()
-}
 
 fn main() {
     let examples: [&dyn Example; 6] = [&W, &Iota, &TailedE, &Biohazard, &Ichi, &StressedE];
- 
+
     for example in examples {
-        let dur = bench(example);
-        println!("{BENCH_COUNT} {}: {} s, {} ns, {} ms", example.name(),
-            dur.as_secs(), dur.as_nanos(), dur.as_millis());
-        println!("1 {}: {} ns, {} µs", example.name(), dur.as_nanos() / BENCH_COUNT as u128,
-            dur.as_micros() as f32 / BENCH_COUNT as f32);
+        example.create_image();
     }
-} 
+}
