6cd915de29480e4b5e7b92d6dd5dfef1d8129003
alexanderved <avedehin692@gmail.com>
2023-04-08 20:34:02 +0300

    Add documentation to bounds.rs

--- src/delaunay/bounds.rs
+++ src/delaunay/bounds.rs
@@ -2,14 +2,17 @@ use crate::point::PointId;
 
 use std::convert;
 
+/// The ids of the points which bound the [`Delaunay`].
 #[derive(Debug, Clone, Copy)]
 pub struct Bounds([PointId; 4]);
 
 impl Bounds {
+    /// Creates a new [`Bounds`] from the given `point_ids`.
     pub fn new(point_ids: [PointId; 4]) -> Self {
         Self(point_ids)
     }
 
+    /// Checks if [`Bounds`] contains the given `point_id`.
     pub fn contains(&self, point_id: PointId) -> bool {
         self.0.contains(&point_id)
     }
