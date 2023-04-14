394094309234771d2e17ceaff7a24c610caf99a3
alexanderved <avedehin692@gmail.com>
2023-04-14 21:16:56 +0300

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,14 @@
 //!
 //! // Create a font
 //! let font_data = include_bytes!("/usr/share/fonts/truetype/open-sans/OpenSans-Regular.ttf");
-//! let mut font = Font::from_vec(font_data.to_vec())?;
+//! let mut font = Font::from_vec(font_data.to_vec()).unwrap();
 //!
 //! // Obtain a glyph
 //! let glyph = font.glyph('a');
 //! // Outline the glyph
 //! let outlined_glyph = font.outline_glyph(glyph);
 //! // Triangulate th glyph
-//! let triangulated_glyph = font.triangulate_glyph(outlined_glyph)?;
+//! let triangulated_glyph = font.triangulate_glyph(outlined_glyph).unwrap();
 //!
 //! // Use the resulting triangulation
 //! triangulated_glyph
